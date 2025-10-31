efa3b56d84d70591cd058b235c0f859b8214851f
jittawee <jtwe.esr@gmail.com>
2025-10-31 16:32:50 +0700

    fix server.js

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ import limiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
+const isProd = process.env.NODE_ENV === "production";
+
 const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://unboxian-js-personal-project.vercel.app",
+    "https://unboxian-js-personal-project.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
